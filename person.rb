431959173b94be6be63ca8a2476ea5ff90df0b1b
Mesi <mse212002@gmail.com>
2022-09-27 12:34:21 +0300

    :rotating_light: fix rubocop errors

--- person.rb
+++ person.rb
@@ -21,8 +21,3 @@ class Person
     age >= 18
   end
 end
-
-# Private method is_of_age? that returns true if @age is greater or equal to 18
-# and false otherwise.
-# Public method can_use_services? that returns true if person is of age or if
-# they have permission from parents.
