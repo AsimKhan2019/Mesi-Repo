83927595757080cb2af08a5757a33265c50ba30b
Mesi <mse212002@gmail.com>
2022-09-27 12:51:25 +0300

    :rotating_light: fix rubocop errors

--- student.rb
+++ student.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: true
+
 require './person'
 
+# implement the Student class
 class Student < Person
-    attr_accessor :classroom
+  attr_accessor :classroom
 
-    def initialize(age, classroom, parent_permission: true, name: 'Unknown')
-        super(age, name, parent_permission)
-        @classroom = classroom
-    end
+  def initialize(age, classroom, parent_permission: true, name: 'Unknown')
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
 
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: true
+
 require './person'
 
+# implement the Teacher class
 class Teacher < Person
-    attr_accessor :specialization
-    
-    def initialize(age, specialization, parent_permission: true, name: 'Unknown')
-        super(age, name, parent_permission)
-        @specialization = specialization
-    end
+  attr_accessor :specialization
 
-    def can_use_services?
-        true
-    end
+  def initialize(age, specialization, parent_permission: true, name: 'Unknown')
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
 end
