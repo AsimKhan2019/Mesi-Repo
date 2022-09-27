2c062fbefd2f3ef3caef9092d57d597a7cfe2164
Mesi <mse212002@gmail.com>
2022-09-27 17:28:35 +0300

    :construction: add book and rental files and corresponding classes, :sparkles: create associations between classes

--- student.rb
+++ student.rb
@@ -1,12 +1,16 @@
 require './person'
 
-# implement the Student class
 class Student < Person
   attr_accessor :classroom
 
   def initialize(age, classroom, parent_permission: true, name: 'Unknown')
     super(age, name, parent_permission)
     @classroom = classroom
+  end
+
+  def class(clsroom)
+    @classroom = classroom
+    clsroom.students_list.push(self) unless clsroom.students_list.include?(self)
   end
 
   def play_hooky
