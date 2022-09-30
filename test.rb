89479a76a0b54b35ee920ea4a6328a26c0ab4c95
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-09-30 22:44:08 +0500

    Completed Pry Testing

--- test.rb
+++ test.rb
@@ -12,21 +12,29 @@ class Test
     @testrents = []
     @testpeople = []
   end
-
+  
   def add_data
+    binding.pry
     testbook = Book.new('Microverse', 'Asim')
     @testbooks.push(testbook)
-    
-    teststudent = Student.new(30, 'Asim', 'y', nil)
-    @testpeople.push(teststudent) #pry works till here 
-
-    book_index = gets.chomp # this is the wrong id 
-    person_idx = gets.chomp # this is wrong id 
-    rental_date = gets.chomp # this is the right date
-    curr_rent = Rental.new(rental_date, @testpeople[person_idx.to_i], @testbooks[book_index.to_i])
-    @testrents.push(curr_rent)
+    binding.pry
+    teststudent = Student.new(30, 'Asim', 'y', 5)
+    @testpeople.push(teststudent)
+    book_index = 3
+    if @testbooks[book_index.to_i]
+      person_idx = 3
+      if @people[person_idx.to_i]
+        rental_date = '2022-03-04'
+        curr_rent = Rental.new(rental_date, @testpeople[person_idx.to_i], @testbooks[book_index.to_i])
+        @testrents.push(curr_rent)
+      else
+        puts 'Enter in a correct person id and try again'
+      end
+    else
+      puts 'Enter in a correct book id and try again'
+    end
   end
 end
-binding.pry
 
 test = Test.new
+test.add_data
