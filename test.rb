81a9906606f4d358274d18725be87f09bf7687de
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-09-30 21:14:14 +0500

    Found a bug

--- test.rb
+++ test.rb
@@ -1,10 +1,10 @@
+require 'pry'
 require './person'
 require './student'
 require './teacher'
 require './book'
 require './rental'
 require './helpers/validators'
-require 'pry'
 
 class Test
   def initialize()
@@ -14,23 +14,19 @@ class Test
   end
 
   def add_data
-    testbook = Book.new
+    testbook = Book.new('Microverse', 'Asim')
     @testbooks.push(testbook)
     
-    teststudent = Student.new
-    @testpeople.push(teststudent)
+    teststudent = Student.new(30, 'Asim', 'y', nil)
+    @testpeople.push(teststudent) #pry works till here 
 
-    puts "\nSelect a book from the following list by number"
-    put 'Get Book index'
-    book_index = 3
-    puts "\nSelect a person from the following list by number"
-    put 'Get person index'
-    person_idx = 3
-    print "\n Date(yyyy/mm/dd): "
-    rental_date = gets.chomp
+    book_index = gets.chomp # this is the wrong id 
+    person_idx = gets.chomp # this is wrong id 
+    rental_date = gets.chomp # this is the right date
     curr_rent = Rental.new(rental_date, @testpeople[person_idx.to_i], @testbooks[book_index.to_i])
     @testrents.push(curr_rent)
-    puts 'Rental added successfully'
   end
   binding.pry
 end
+
+test = Test.new
